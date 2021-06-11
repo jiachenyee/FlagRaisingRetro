ff5d29b6e52cd9d07f03534b3f6338975a7a1cb5
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-12 05:27:52 +0800

    Initial Commit

---
+++ FlagRaisingRetro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlagRaisingRetro
+//
+//  Created by JiaChen(: on 12/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlagRaisingRetro/FlagRaisingRetroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagRaisingRetroApp.swift
+//  FlagRaisingRetro
+//
+//  Created by JiaChen(: on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FlagRaisingRetroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
