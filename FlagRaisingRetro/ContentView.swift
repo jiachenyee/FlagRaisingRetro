5d8f48dd44b006ef0060beec203874f36fabd245
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-12 06:15:44 +0800

    did up retro finder

--- FlagRaisingRetro/ContentView.swift
+++ FlagRaisingRetro/ContentView.swift
@@ -8,9 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var offset: CGFloat = 30
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .top) {
+            Color.white.border(Color.black, width: 2)
+            VStack(spacing: -2) {
+                ZStack(alignment: .leading) {
+                    HStack(spacing: 0) {
+                        VStack(spacing: 2) {
+                            ForEach(0..<6) { _ in
+                                Rectangle()
+                                    .background(Color.black)
+                                    .frame(height: 2)
+                            }
+                        }
+                        Text("Flag Raising")
+                            .font(.system(size: 24, weight: .heavy, design: .monospaced))
+                            .padding(.horizontal, 5)
+                        VStack(spacing: 2) {
+                            ForEach(0..<6) { _ in
+                                Rectangle()
+                                    .background(Color.black)
+                                    .frame(height: 2)
+                            }
+                        }
+                    }
+                    .padding(5)
+                    .border(Color.black, width: 2)
+                    
+                    Rectangle()
+                        .stroke(lineWidth: 2)
+                        .frame(width: 26, height: 22)
+                        .background(Color.white)
+                        .padding(.leading, 22)
+                        .foregroundColor(.white)
+                    
+                    Rectangle()
+                        .stroke(lineWidth: 2)
+                        .frame(width: 22, height: 22)
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .padding(.leading, 24)
+                        .onTapGesture {
+                            NSApplication.shared.terminate(nil)
+                        }
+                }
+                
+                HStack(alignment: .bottom, spacing: -10) {
+                    Rectangle()
+                        .frame(width: 10)
+                    Text("🇸🇬")
+                        .font(.system(size: 100))
+                        .offset(x: 0, y: offset)
+                    
+                }
+                .padding()
+                
+                ZStack {
+                    Color.black
+                        .offset(x: 2, y: 2)
+                    Color.white
+                        .border(Color.black, width: 2)
+                    
+                    Text("Raise")
+                        .font(.system(size: 20, weight: .heavy, design: .monospaced))
+                }
+                .frame(width: 150, height: 50)
+                .onTapGesture {
+                    offset -= 10
+                }
+                .padding()
+            }
+        }
+        .colorScheme(.light)
     }
 }
 
--- FlagRaisingRetro/FlagRaisingRetroApp.swift
+++ FlagRaisingRetro/FlagRaisingRetroApp.swift
@@ -13,5 +13,6 @@ struct FlagRaisingRetroApp: App {
         WindowGroup {
             ContentView()
         }
+        .windowStyle(HiddenTitleBarWindowStyle())
     }
 }
